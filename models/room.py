b9b7d60084ae187cb1f51a4bac7ff3a97a65d118
EinandererUser <noreply@pyloxis.net>
2016-09-23 10:53:18 +0000

    ADDED: Comments describing database relationships.

--- models/room.py
+++ models/room.py
@@ -2,8 +2,9 @@ from database import Base
 from sqlalchemy import Table, Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 
-__author__ = 'ohaz'
+__author__ = 'ohaz, PopUp'
 
+# room_transit table of PARENT-CHILD many-to-many relationship of Room to Room
 room_transit = Table('room_transit', Base.metadata,
     Column('entry_id', Integer, ForeignKey('room.id'), primary_key=True),
     Column('exit_id', Integer, ForeignKey('room.id'), primary_key=True),
@@ -13,11 +14,16 @@ room_transit = Table('room_transit', Base.metadata,
 class Room(Base):
     __tablename__ = 'room'
     id = Column(Integer, primary_key=True)
-    desc_name = Column(String(32))
-    desc_flavor = Column(String(192))
     generated = Column(Boolean(), default=False)
+
+    # PARENT-CHILD many-to-many relationship to Room.entries using the room_transit table
     exits = relationship("Room", secondary=room_transit, primaryjoin=id==room_transit.c.entry_id, secondaryjoin=id==room_transit.c.exit_id, backref="entries")
 
+    # Room name with the preceding article and the describing flavor text
+    desc_name = Column(String(32))    # e.g. "in the temple", "at the docks."
+    desc_flavor = Column(String(192)) # e.g. "The stench of decay assaults your nose.", "The trees sway in the wind."
+
+    # PARENT-Child one-to-many relationship to character
     characters = relationship("Character", back_populates="room")
 
     def get_description(self):
